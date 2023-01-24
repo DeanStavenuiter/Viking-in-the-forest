bf3dedd686834830be4bca5b4728fa92e9ad721c
DeanStavenuiter <info@deanstavenuiter.nl>
2023-01-24 15:12:58 +0100

    added moving clouds and monsters

--- index.js
+++ index.js
@@ -2,14 +2,43 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
 const gravity = 0.5;
-const gameOver = false;
+let gameOver = false;
 let animateId;
+
+//keys
+const keys = {
+  d: {
+    pressed: false,
+  },
+  a: {
+    pressed: false,
+  },
+  w: {
+    pressed: false,
+  },
+  arrowRight: {
+    pressed: false,
+  },
+  arrowLeft: {
+    pressed: false,
+  },
+  arrowUp: {
+    pressed: false,
+  },
+};
 
 //function create image
 function createImage(imageSrc) {
   const image = new Image();
   image.src = imageSrc;
   return image;
+}
+
+//random number function
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
 }
 
 //background class
@@ -64,8 +93,6 @@ class Player {
   //draw player
   draw() {
     ctx.drawImage(this.image, this.position.x, this.position.y);
-    // ctx.fillStyle = "red";
-    // ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
   }
 
   update() {
@@ -98,11 +125,16 @@ class Platform {
   }
 }
 
+//object class
 class Objects {
   constructor(x, y, width, height, image) {
     this.position = {
       x,
       y,
+    };
+    this.velocity = {
+      x: getRandomInt(0, -3),
+      y: 0,
     };
     this.image = image;
     this.width = width;
@@ -112,15 +144,44 @@ class Objects {
   draw() {
     ctx.drawImage(this.image, this.position.x, this.position.y);
   }
+  update() {
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+  }
+}
+
+//monster class
+class Monsters {
+  constructor(x, y, width, height, image) {
+    this.position = {
+      x,
+      y,
+    };
+    this.velocity = {
+      x: 0,
+      y: 0,
+    };
+    this.image = image;
+    this.width = width;
+    this.height = height;
+  }
+
+  draw() {
+    ctx.drawImage(this.image, this.position.x, this.position.y);
+  }
+  update() {
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+  }
 }
 
 //create background
 let backgroundGame = new Background({
   position: {
     x: 0,
-    y: -576,
+    y: -517,
   },
-  imgSrc: "/img/level 1 img/background.png",
+  imgSrc: "/img//level 1 img/background.png",
 });
 
 //create a player with his starting positions
@@ -141,7 +202,7 @@ let isMovingDown = false;
 let isNotMoving = true;
 let wasRight = true;
 let wasLeft = false;
-let imgChar = 1
+let imgChar = 1;
 // let damage = false;
 
 //big platforms
@@ -162,6 +223,51 @@ const floatingPlatform = createImage("/img/level 1 img/floating-platform.png");
 const floatingSingleBlock = createImage(
   "img/level 1 img/single-floating-block.png"
 );
+//door
+const door = createImage("/img/level 1 img/door-closed.png");
+
+//clouds
+const cloud1 = createImage("img/clouds/cloud_shape2_1.png");
+const cloud2 = createImage("img/clouds/cloud_shape2_2.png");
+const cloud3 = createImage("img/clouds/cloud_shape2_3.png");
+const cloud4 = createImage("img/clouds/cloud_shape2_4.png");
+const cloud5 = createImage("img/clouds/cloud_shape3_1.png");
+const cloud6 = createImage("img/clouds/cloud_shape3_2.png");
+const cloud7 = createImage("img/clouds/cloud_shape3_3.png");
+const cloud8 = createImage("img/clouds/cloud_shape3_4.png");
+const cloud9 = createImage("img/clouds/cloud_shape3_5.png");
+const cloud10 = createImage("img/clouds/cloud_shape4_1.png");
+const cloud11 = createImage("img/clouds/cloud_shape4_2.png");
+const cloud12 = createImage("img/clouds/cloud_shape4_3.png");
+const cloud13 = createImage("img/clouds/cloud_shape4_4.png");
+const cloud14 = createImage("img/clouds/cloud_shape4_5.png");
+const cloud15 = createImage("img/clouds/cloud_shape6_1.png");
+const cloud16 = createImage("img/clouds/cloud_shape6_2.png");
+const cloud17 = createImage("img/clouds/cloud_shape6_3.png");
+const cloud18 = createImage("img/clouds/cloud_shape6_4.png");
+const cloud19 = createImage("img/clouds/cloud_shape6_5.png");
+
+let clouds = [
+  new Objects(getRandomInt(0, 3778), getRandomInt(0, 200), 3778, 308, cloud1),
+  new Objects(getRandomInt(0, 3778), getRandomInt(0, 200), 3778, 308, cloud2),
+  new Objects(getRandomInt(0, 3778), getRandomInt(0, 200), 3778, 308, cloud3),
+  new Objects(getRandomInt(0, 3778), getRandomInt(0, 200), 3778, 308, cloud4),
+  new Objects(getRandomInt(0, 3778), getRandomInt(0, 200), 3778, 308, cloud5),
+  new Objects(getRandomInt(0, 3778), getRandomInt(0, 200), 3778, 308, cloud6),
+  new Objects(getRandomInt(0, 3778), getRandomInt(0, 200), 3778, 308, cloud7),
+  new Objects(getRandomInt(0, 3778), getRandomInt(0, 200), 3778, 308, cloud8),
+  new Objects(getRandomInt(0, 3778), getRandomInt(0, 200), 3778, 308, cloud9),
+  new Objects(getRandomInt(0, 3778), getRandomInt(0, 200), 3778, 308, cloud10),
+  new Objects(getRandomInt(0, 3778), getRandomInt(0, 200), 3778, 308, cloud11),
+  new Objects(getRandomInt(0, 3778), getRandomInt(0, 200), 3778, 308, cloud12),
+  new Objects(getRandomInt(0, 3778), getRandomInt(0, 200), 3778, 308, cloud13),
+  new Objects(getRandomInt(0, 3778), getRandomInt(0, 200), 3778, 308, cloud14),
+  new Objects(getRandomInt(0, 3778), getRandomInt(0, 200), 3778, 308, cloud15),
+  new Objects(getRandomInt(0, 3778), getRandomInt(0, 200), 3778, 308, cloud16),
+  new Objects(getRandomInt(0, 3778), getRandomInt(0, 200), 3778, 308, cloud17),
+  new Objects(getRandomInt(0, 3778), getRandomInt(0, 200), 3778, 308, cloud18),
+  new Objects(getRandomInt(0, 3778), getRandomInt(0, 200), 3778, 308, cloud19),
+];
 
 //array of platforms
 let bigPlatforms = [
@@ -170,32 +276,28 @@ let bigPlatforms = [
   new Platform(1344, 389, 327, 192, higherPlatformSmall),
   new Platform(1153, 453, 319, 128, highPlatformSmall),
   new Platform(1724, 453, 444, 128, highPlatformBig),
-  new Platform(2296, 325, 64, 64, floatingSingleBlock),
-  new Platform(2424, 261, 64, 64, floatingSingleBlock),
-  new Platform(2552, 453, 64, 64, floatingSingleBlock),
-  new Platform(2680, 325, 64, 64, floatingSingleBlock),
+  new Platform(2296, 325, 34, 64, floatingSingleBlock),
+  new Platform(2424, 261, 34, 64, floatingSingleBlock),
+  new Platform(2552, 453, 34, 64, floatingSingleBlock),
+  new Platform(2680, 325, 34, 64, floatingSingleBlock),
   new Platform(2808, 453, 444, 128, highPlatformBig),
-  new Platform(3316, 325, 64, 64, floatingSingleBlock),
-  new Platform(3444, 133, 64, 64, floatingSingleBlock),
-  new Platform(3316, 0, 64, 64, floatingSingleBlock),
-  // new Platform(3444, 133, 64, 64, floatingSingleBlock),
-  // new Platform(3316, 69, 64, 64, floatingSingleBlock),
-  // new Platform(3444, 5, 64, 64, floatingSingleBlock),
-  // new Platform(3572, 69, 64, 64, floatingSingleBlock),
-  new Platform(3700, 453, 192, 64, floatingPlatform),
+  new Platform(3316, 325, 34, 64, floatingSingleBlock),
+  new Platform(3444, 133, 34, 64, floatingSingleBlock),
+  new Platform(3732, 453, 192, 64, floatingPlatform),
+  new Platform(3850, 382, 64, 64, door),
 ];
 
-function reset() {
-  //create background
-  backgroundGame = new Background({
-    position: {
-      x: 0,
-      y: -576,
-    },
-    imgSrc: "/img/level 1 img/background.png",
-  });
+//monsters
+const monster1Image = createImage("/img/animations/monster1/monsterleft1.png");
 
-  //create a player with his starting positions
+let monsters = [
+  new Monsters(600, 330, 120, 120, monster1Image),
+  new Monsters(700, 330, 120, 120, monster1Image),
+];
+
+//reset function for when you die
+function reset() {
+  
   player = new Player(
     {
       x: 0,
@@ -204,7 +306,7 @@ function reset() {
     knightImg
   );
 
-  //array of platforms
+  
   bigPlatforms = [
     new Platform(0, 453, 444, 128, highPlatformBig),
     new Platform(581, 453, 444, 128, highPlatformBig),
@@ -218,38 +320,98 @@ function reset() {
     new Platform(2808, 453, 444, 128, highPlatformBig),
     new Platform(3316, 325, 64, 64, floatingSingleBlock),
     new Platform(3444, 133, 64, 64, floatingSingleBlock),
-    new Platform(3316, 0, 64, 64, floatingSingleBlock),
-    // new Platform(3444, 133, 64, 64, floatingSingleBlock),
-    // new Platform(3316, 69, 64, 64, floatingSingleBlock),
-    // new Platform(3444, 5, 64, 64, floatingSingleBlock),
-    // new Platform(3572, 69, 64, 64, floatingSingleBlock),
     new Platform(3700, 453, 192, 64, floatingPlatform),
+    new Platform(3850, 382, 64, 64, door),
   ];
 
+  clouds = [
+    new Objects(getRandomInt(0, 3778), getRandomInt(0, 200), 3778, 308, cloud1),
+    new Objects(getRandomInt(0, 3778), getRandomInt(0, 200), 3778, 308, cloud2),
+    new Objects(getRandomInt(0, 3778), getRandomInt(0, 200), 3778, 308, cloud3),
+    new Objects(getRandomInt(0, 3778), getRandomInt(0, 200), 3778, 308, cloud4),
+    new Objects(getRandomInt(0, 3778), getRandomInt(0, 200), 3778, 308, cloud5),
+    new Objects(getRandomInt(0, 3778), getRandomInt(0, 200), 3778, 308, cloud6),
+    new Objects(getRandomInt(0, 3778), getRandomInt(0, 200), 3778, 308, cloud7),
+    new Objects(getRandomInt(0, 3778), getRandomInt(0, 200), 3778, 308, cloud8),
+    new Objects(getRandomInt(0, 3778), getRandomInt(0, 200), 3778, 308, cloud9),
+    new Objects(
+      getRandomInt(0, 3778),
+      getRandomInt(0, 200),
+      3778,
+      308,
+      cloud10
+    ),
+    new Objects(
+      getRandomInt(0, 3778),
+      getRandomInt(0, 200),
+      3778,
+      308,
+      cloud11
+    ),
+    new Objects(
+      getRandomInt(0, 3778),
+      getRandomInt(0, 200),
+      3778,
+      308,
+      cloud12
+    ),
+    new Objects(
+      getRandomInt(0, 3778),
+      getRandomInt(0, 200),
+      3778,
+      308,
+      cloud13
+    ),
+    new Objects(
+      getRandomInt(0, 3778),
+      getRandomInt(0, 200),
+      3778,
+      308,
+      cloud14
+    ),
+    new Objects(
+      getRandomInt(0, 3778),
+      getRandomInt(0, 200),
+      3778,
+      308,
+      cloud15
+    ),
+    new Objects(
+      getRandomInt(0, 3778),
+      getRandomInt(0, 200),
+      3778,
+      308,
+      cloud16
+    ),
+    new Objects(
+      getRandomInt(0, 3778),
+      getRandomInt(0, 200),
+      3778,
+      308,
+      cloud17
+    ),
+    new Objects(
+      getRandomInt(0, 3778),
+      getRandomInt(0, 200),
+      3778,
+      308,
+      cloud18
+    ),
+    new Objects(
+      getRandomInt(0, 3778),
+      getRandomInt(0, 200),
+      3778,
+      308,
+      cloud19
+
+    ),
+  ];
+    monsters = [
+        new Monsters(600, 330, 120, 120, monster1Image),
+        new Monsters(700, 330, 120, 120, monster1Image),
+      ];
   scrollOfset = 0;
 }
-
-//keys
-const keys = {
-  d: {
-    pressed: false,
-  },
-  a: {
-    pressed: false,
-  },
-  w: {
-    pressed: false,
-  },
-  arrowRight: {
-    pressed: false,
-  },
-  arrowLeft: {
-    pressed: false,
-  },
-  arrowUp: {
-    pressed: false,
-  },
-};
 
 //array of hearts
 const heartImg = createImage("/img/level 1 img/heart.png");
@@ -260,8 +422,17 @@ let hearts = [
   new Hearts(950, 10, 30, 30, heartImg),
 ];
 
-//checks for the end of the game
+//animation settings
 let scrollOfset = 0;
+let idleRight = 1;
+let idleLeft = 1;
+let runRight = 1;
+let runLeft = 1;
+let jumpRight = 1;
+let jumpLeft = 1;
+let monster1 = 1;
+let monster1WasRight = true;
+let monster1WasLeft = true;
 
 //animate function
 function animate() {
@@ -270,6 +441,12 @@ function animate() {
 
   //background
   backgroundGame.update();
+
+  //clouds
+  clouds.forEach((cloudImg) => {
+    cloudImg.draw();
+    cloudImg.update();
+  });
 
   //platforms
   bigPlatforms.forEach((platform) => {
@@ -280,79 +457,127 @@ function animate() {
   hearts.forEach((heart) => {
     heart.draw();
   });
+
+  //monsters
+  monsters.forEach((monster) => {
+    monster.draw();
+
+    //monster moving left
+    if (monster1WasRight) {
+      monster1Image.src = "/img/animations/monster1/monsterleft1.png";
+      if (animateId % 20 === 0) {
+        monster1 += 1;
+        if (monster1 > 6) {
+          monster1 = 1;
+        }
+      }
+      monster1Image.src =
+        "/img/animations/monster1/monsterleft" + monster1 + ".png";
+    }
+
+    //monster moving right
+    if (monster1WasLeft) {
+      monster1Image.src = "/img/animations/monster1/monsterright1.png";
+      if (animateId % 20 === 0) {
+        monster1 += 1;
+        if (monster1 > 6) {
+          monster1 = 1;
+        }
+      }
+      monster1Image.src =
+        "/img/animations/monster1/monsterright" + monster1 + ".png";
+    }
+  });
+
   //players
   player.update();
 
   //idle right
-  if (wasRight && isNotMoving) {
-    // knightImg.src = "/img/animations/knight/idle/idleright1.png";
+  if (isNotMoving && wasRight) {
+    knightImg.src = "/img/animations/knight/idle/idleright1.png";
     if (animateId % 10 === 0) {
-      imgChar += 1;
-      if (imgChar > 12) {
-        imgChar = 1;
+      idleRight += 1;
+      if (idleRight > 12) {
+        idleRight = 1;
       }
     }
-    knightImg.src = "/img/animations/knight/idle/idleright" + imgChar + ".png";
+    knightImg.src =
+      "/img/animations/knight/idle/idleright" + idleRight + ".png";
   }
 
   //idle left
-  if (wasLeft && isNotMoving) {
+  if (isNotMoving && wasLeft) {
     knightImg.src = "/img/animations/knight/idle/idleleft1.png";
     if (animateId % 10 === 0) {
-      imgChar += 1;
-      if (imgChar > 12) {
-        imgChar = 1;
+      idleLeft += 1;
+      if (idleLeft > 12) {
+        idleLeft = 1;
       }
     }
-    knightImg.src = "/img/animations/knight/idle/idleleft" + imgChar + ".png";
+    knightImg.src = "/img/animations/knight/idle/idleleft" + idleLeft + ".png";
   }
 
   //run right
-  if (isMovingRight) {
+  if (
+    (keys.d.pressed && isMovingRight) ||
+    (keys.arrowRight.pressed && isMovingRight)
+  ) {
     knightImg.src = "/img/animations/knight/walk/walkright1.png";
     if (animateId % 10 === 0) {
-      imgChar += 1;
-      if (imgChar === 6) {
-        imgChar = 1;
+      runRight += 1;
+      if (runRight === 6) {
+        runRight = 1;
       }
     }
-    knightImg.src = "/img/animations/knight/walk/walkright" + imgChar + ".png";
+    knightImg.src = "/img/animations/knight/walk/walkright" + runRight + ".png";
   }
 
   //run left
-  if (isMovingLeft) {
+  if (
+    (keys.a.pressed && isMovingLeft) ||
+    (keys.arrowLeft.pressed && isMovingLeft)
+  ) {
     knightImg.src = "/img/animations/knight/walk/walkleft1.png";
     if (animateId % 10 === 0) {
-      imgChar += 1;
-      if (imgChar === 6) {
-        imgChar = 1;
+      runLeft += 1;
+      if (runLeft === 6) {
+        runLeft = 1;
       }
     }
-    knightImg.src = "/img/animations/knight/walk/walkleft" + imgChar + ".png";
+    knightImg.src = "/img/animations/knight/walk/walkleft" + runLeft + ".png";
   }
 
   //jump right
-  if (wasRight && isMovingUp) {
+  if (
+    (wasRight && isMovingUp) ||
+    (wasRight && isMovingUp && keys.d.pressed) ||
+    (wasRight && isMovingUp && keys.arrowRight.pressed)
+  ) {
     knightImg.src = "/img/animations/knight/jump/jumpright1.png.png";
     if (animateId % 10 === 0) {
-      imgChar += 1;
-      if (imgChar === 7) {
-        imgChar = 1;
+      jumpRight += 1;
+      if (jumpRight === 7) {
+        jumpRight = 1;
       }
     }
-    knightImg.src = "/img/animations/knight/jump/jumpright" + imgChar + ".png";
+    knightImg.src =
+      "/img/animations/knight/jump/jumpright" + jumpRight + ".png";
   }
 
   //jump left
-  if (wasRight && isMovingUp) {
-    // knightImg.src = "/img/animations/knight/jump/jumpright1.png.png";
+  if (
+    (wasLeft && isMovingUp) ||
+    (wasLeft && isMovingUp && keys.a.pressed) ||
+    (wasLeft && isMovingUp && keys.arrowLeft.pressed)
+  ) {
+    knightImg.src = "/img/animations/knight/jump/jumpleft1.png.png";
     if (animateId % 10 === 0) {
-      imgChar += 1;
-      if (imgChar === 7) {
-        imgChar = 1;
+      jumpLeft += 1;
+      if (jumpLeft === 7) {
+        jumpLeft = 1;
       }
     }
-    knightImg.src = "/img/animations/knight/jump/jumpright" + imgChar + ".png";
+    knightImg.src = "/img/animations/knight/jump/jumpleft" + jumpLeft + ".png";
   }
 
   //speed of moving left and right
@@ -374,11 +599,20 @@ function animate() {
     if (keys.d.pressed || keys.arrowRight.pressed) {
       scrollOfset += 2;
 
+      monsters.forEach(monster => {
+        monster.position.x -=2
+      })
+
       bigPlatforms.forEach((platform) => {
         platform.position.x -= 2;
       });
     } else if (keys.a.pressed || keys.arrowLeft.pressed) {
       scrollOfset -= 2;
+
+
+      monsters.forEach(monster => {
+        monster.position.x +=2
+      })
 
       bigPlatforms.forEach((platform) => {
         platform.position.x += 2;
@@ -389,8 +623,8 @@ function animate() {
     if (scrollOfset > 2000) {
       console.log("you win");
     }
-    //lose scenario
 
+    //lose scenario
     if (player.position.y > canvas.height) {
       totalHearts -= 1;
       hearts.pop();
@@ -398,8 +632,8 @@ function animate() {
       console.log(totalHearts);
       console.log("you loose");
     }
-    if (totalHearts === 0){
-      gameOver = true
+    if (totalHearts === 0) {
+      gameOver = true;
     }
   }
 
@@ -409,8 +643,8 @@ function animate() {
       player.position.y + player.height <= platform.position.y &&
       player.position.y + player.height + player.velocity.y >=
         platform.position.y &&
-      player.position.x + player.width >= platform.position.x &&
-      player.position.x <= platform.position.x - 50 + platform.width
+      player.position.x + player.width >= platform.position.x - 20 &&
+      player.position.x <= platform.position.x - 30 + platform.width
     ) {
       player.velocity.y = 0;
     }
@@ -421,7 +655,6 @@ animate();
 //movement tracker
 window.addEventListener("keydown", (event) => {
   switch (event.key) {
-    // console.log(event);
     case "d": //right
       keys.d.pressed = true;
       isMovingRight = true;
@@ -468,8 +701,6 @@ window.addEventListener("keydown", (event) => {
 });
 
 window.addEventListener("keyup", (event) => {
-  isMovingLeft = false;
-  isMovingRight = false;
   isMovingUp = false;
   isMovingDown = false;
   isNotMoving = true;
@@ -477,16 +708,35 @@ window.addEventListener("keyup", (event) => {
   switch (event.key) {
     case "d": //right
       keys.d.pressed = false;
+      isMovingRight = false;
+      wasRight = true;
+      wasLeft = false;
       break;
     case "a": //left
       keys.a.pressed = false;
+      isMovingLeft = false;
+      wasRight = false;
+      wasLeft = true;
+      break;
+    case "w": //up
+      keys.w.pressed = false;
+      isMovingUp = false;
       break;
     case "ArrowRight": //right
       keys.arrowRight.pressed = false;
+      isMovingRight = false;
+      wasRight = true;
+      wasLeft = false;
       break;
     case "ArrowLeft": //left
       keys.arrowLeft.pressed = false;
+      isMovingLeft = false;
+      wasRight = false;
+      wasLeft = true;
       break;
+    case "ArrowUp": //up
+      keys.arrowUp.pressed = false;
+      isMovingUp = false;
   }
 });
 
